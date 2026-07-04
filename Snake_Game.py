36daec191ed555e970be461c49b0a226550a11ba
Hooria Sheema Sami <hooriasheemasami@gmail.com>
2026-07-04 12:13:12 +0500

    Added Fruit class and rendering

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,14 +1,33 @@
 import pygame, sys
+from pygame.math import Vector2
+
+class FRUIT: 
+    def __init__(self): # Create an x and y position
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x, self.y)
+        
+    def draw_fruit(self): # Draw a square which will be the fruit
+        fruit_rect = pygame.Rect(int(self.pos.x  * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)  # Create a rectangle for the fruit
+        pygame.draw.rect(screen, (126, 166, 113), fruit_rect)
+
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 30
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
+
+fruit = FRUIT()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+            
+    screen.fill((180, 75, 200)) # Decide the screen color
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)  # Limit the frame rate to 60 FPS
-    
+     
